c9b8d9d24af9d45e2c8a21567ef2328f90fc70f4
rex-vero <142157808+rex-vero@users.noreply.github.com>
2023-11-13 21:18:50 +0330

    scss for css & js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,20 @@ const ball = document.getElementById('ball');
 const area = document.getElementById('area');
 const score = document.getElementById('score');
 const number = document.getElementById('number');
+const prosses = document.getElementById('prosses');
 area.addEventListener('mousemove', (e) => {
     ball.style.top = e.pageY - 40 + 'px';
     ball.style.left = e.pageX - 40 + 'px';
 });
 score.addEventListener('mouseover', () => {
-    setTimeout(() => {
-        console.log('drfgbrh');
+    prosses.style.width = 100 + '%';
+    prosses.classList.add('transition')
+    time = setTimeout(() => {
         number.innerText++;
     }, 5000);
-
 });
 score.addEventListener('mouseout', () => {
-    clearTimeout(score);
+    prosses.classList.remove('transition')
+    prosses.style.width = '';
+    clearTimeout(time);
 });
